053af02d31ce1e488575f63f90d4714b3f29a2f7
walton-alex <webdev.walton@gmail.com>
2018-06-25 10:20:21 +0100

    Tidying up and some amends

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const displayArea = document.querySelector('.bookmarks');
 
 let bookmarkItems = [];
 
-// Read LocalStorage
+// Pulls in any saved bookmarks from localstorage
 if (localStorage.getItem('Bookmarks')) {
   const readLocalStorage = localStorage.getItem('Bookmarks');
   bookmarkItems = JSON.parse(readLocalStorage);
@@ -26,33 +26,33 @@ bookmarkItems.forEach(bookmarkInput => {
 bookmark.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Variables & Data Capture
+  // Capture user input data, and apply them to variables
   let bookmarkSiteName = document.querySelector('.container__section__name').value;
   let bookmarkUrl = document.querySelector('.container__section__url').value;
   let url;
 
   // Validation of Site Name
-  if (bookmarkSiteName === '') {
+  if (bookmarkSiteName.trim() === '') {
     bookmarkNameInput.classList.add('container__section__name--error');
   }
 
-  // Validation of URL - Try / Catch
+  // Validation of URL - Using Try / Catch
   try {
     url = new URL(bookmarkUrl);
   } catch(error) {
     bookmarkUrlInput.classList.add('container__section__url--error');
   }
 
-  if (url == undefined || bookmarkSiteName === '') {
+  if (url == undefined || bookmarkSiteName.trim() === '') {
     bookmark.parentElement.classList.add('container__section--error');
     return;
   }
 
   // Values to output
-  const outputName = document.querySelector('.container__output__name').value = bookmarkSiteName;
-  const outputUrl = document.querySelector('.container__output__url').value = bookmarkUrl;
+  document.querySelector('.container__output__name').value = bookmarkSiteName;
+  document.querySelector('.container__output__url').value = bookmarkUrl;
 
-  // Create object
+  // Create object that will be pushed to an array, and used with localstorage
   const bookmarkObj = {
     sitename: bookmarkSiteName,
     siteurl: bookmarkUrl,
@@ -60,23 +60,20 @@ bookmark.addEventListener('submit', (e) => {
   }
 
   // Take ID and assign it to the ID attribute
-  const outputID = document.querySelector('.container__output').setAttribute('data-unique-number', bookmarkObj.id);
+  document.querySelector('.container__output').setAttribute('data-unique-number', bookmarkObj.id);
 
-  // Output
+  // Clone the invisble template, captured data already included
   let outputClone = output.cloneNode(true);
   outputClone.style.display = 'block';
   displayArea.appendChild(outputClone);
 
-  // Push to array
+  // Push this bookmark to an array used with localstorage
   bookmarkItems.push(bookmarkObj);
 
-  // JSON.stringify
   const objToString = JSON.stringify(bookmarkItems);
-
-  // Localstorage
   localStorage.setItem('Bookmarks', objToString);
 
-  // Clear
+  // Clear the input fields
   document.querySelector('.container__section__name').value = '';
   document.querySelector('.container__section__url').value = '';
   document.querySelector('.container__output').setAttribute('data-unique-number', '');
@@ -84,27 +81,24 @@ bookmark.addEventListener('submit', (e) => {
 
 // Output buttons
 // Go!
-document.addEventListener('click', (e) => {
+document.addEventListener('click', ({target}) => {
   
-  if (e.target && e.target.matches('.container__output__button')) {
-    const outputUrl = e.target.parentElement.querySelector('.container__output__url').value;
-    e.target.setAttribute("href", outputUrl);
+  if (target && target.matches('.container__output__button')) {
+    const outputUrl = target.parentElement.querySelector('.container__output__url').value;
+    target.setAttribute("href", outputUrl);
   }
 
 });
 
 // Delete!
-document.addEventListener('click', (e) => {
+document.addEventListener('click', ({target}) => {
   
-  if (e.target && e.target.matches('.container__output__button-del')) {
-    const parentBookmark = e.target.parentElement.parentElement;
+  if (target && target.matches('.container__output__button-del')) {
+    const parentBookmark = target.parentElement.parentElement;
     parentBookmark.remove();
 
     // Remove from array to remove from localstorage
     const bookmarkID = parentBookmark.getAttribute('data-unique-number');
-
-    // console.log(bookmarkID);
-    // console.log(bookmarkItems);
     
     const match = (arrayItem) => {
 
@@ -116,10 +110,9 @@ document.addEventListener('click', (e) => {
 
     }
 
-    // Filter
     bookmarkItems = bookmarkItems.filter(match);
 
-    // Reset local storage
+    // Reset localstorage
     const objToString = JSON.stringify(bookmarkItems);
     localStorage.setItem('Bookmarks', objToString);
 
@@ -129,23 +122,23 @@ document.addEventListener('click', (e) => {
 
 // Resets
 // Allows animation to fire more than once
-document.addEventListener('animationend', (e) => {
+document.addEventListener('animationend', () => {
   bookmark.parentElement.classList.remove('container__section--error');
 });
 
 // Remove the error if there is user input
-document.addEventListener('input', (e) => {
+document.addEventListener('input', ({target}) => {
 
-  if (e.target && e.target.matches('.container__section__name--error')) {
+  if (target && target.matches('.container__section__name--error')) {
     bookmarkNameInput.classList.remove('container__section__name--error');
   }
 
 });
 
 // Remove the error if there is user input
-document.addEventListener('input', (e) => {
+document.addEventListener('input', ({target}) => {
 
-  if (e.target && e.target.matches('.container__section__url--error')) {
+  if (target && target.matches('.container__section__url--error')) {
     bookmarkUrlInput.classList.remove('container__section__url--error');
   }
 
